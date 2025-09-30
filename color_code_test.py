77e2255ee1a63617d248ec7c829686d509dcc1dc
Venkatesh Thamburapu <37204830+thevenkateshthamburapu@users.noreply.github.com>
2025-09-30 20:14:32 +0530

    Update color_code_test.py

--- color_code_test.py
+++ color_code_test.py
@@ -5,7 +5,7 @@ def test_number_to_pair(pair_number, expected_major_color, expected_minor_color)
     assert major_color == expected_major_color
     assert minor_color == expected_minor_color
 
-defmajor_color, minor_color, expected_pair_number):
+def test_pair_to_number(major_color, minor_color, expected_pair_number):
     pair_number = get_pair_number_from_color(major_color, minor_color)
     assert pair_number == expected_pair_number
 
@@ -15,4 +15,4 @@ if __name__ == '__main__':
     test_pair_to_number('Black', 'Orange', 12)
     test_pair_to_number('Violet', 'Slate', 25)
     test_pair_to_number('Red', 'Orange', 7)
-    print('Done :)')
+    print('All tests passed ✅')
