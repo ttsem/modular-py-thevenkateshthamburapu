c230528b60ebf18da1c7722aac02ca289087d3a2
Venkatesh Thamburapu <37204830+thevenkateshthamburapu@users.noreply.github.com>
2025-09-30 20:18:11 +0530

    Update color_code_constants.py

--- color_code_constants.py
+++ color_code_constants.py
@@ -1,4 +1,4 @@
 # constants - major and minor color
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS = ['Blue', 'Orange', 'Green', 'Brown', 'S
+MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
